e96469e120139ceccf0c29629f1b95694667f1ee
iqrasarfaraz <160213971+iqrasarfaraz@users.noreply.github.com>
2024-05-10 22:55:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// mathematical task
+//(x+y*z)/(w-1)-(y-w*x)*x+z/w
+var w = 2;
+var x = 3;
+var y = 5;
+var z = 4;
+// put euation
+var equation1 = (x + y * z) / (w - 1) - (y - w * x) * x + z / w;
+//print answer
+console.log(equation1);
---
+++ main.ts
@@ -0,0 +1,14 @@
+// mathematical task
+//(x+y*z)/(w-1)-(y-w*x)*x+z/w
+
+let w = 2;
+let x = 3;
+let y = 5;
+let z = 4
+
+// put euation
+
+let equation1 = (x + y * z)/(w-1) - (y - w * x) * x + z / w
+
+//print answer
+console.log( equation1) 
